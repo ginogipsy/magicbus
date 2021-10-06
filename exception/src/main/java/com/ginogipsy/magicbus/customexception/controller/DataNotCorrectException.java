42656fe004e685349b406310f30ade89b286ae16
Francesco Grossi <francesco.grossi92@gmail.com>
2021-10-06 11:27:02 +0200

    fix - classi già fatte per renderle migliori

---
+++ exception/src/main/java/com/ginogipsy/magicbus/customexception/controller/DataNotCorrectException.java
@@ -0,0 +1,12 @@
+package com.ginogipsy.magicbus.customexception.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "I dati inseriti non sono corretti!")
+public class DataNotCorrectException extends RuntimeException{
+
+    public DataNotCorrectException(String message) {
+        super(message);
+    }
+}
