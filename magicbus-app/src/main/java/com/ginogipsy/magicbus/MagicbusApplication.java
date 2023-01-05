54fcf3d8daccf4e49c2f05bbc9dd8cd9491c8d6c
ginogipsy <francesco.grossi92@gmail.com>
2023-01-05 18:31:19 +0100

    create first schedule

--- magicbus-app/src/main/java/com/ginogipsy/magicbus/MagicbusApplication.java
+++ magicbus-app/src/main/java/com/ginogipsy/magicbus/MagicbusApplication.java
@@ -4,11 +4,14 @@ import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableEncryptableProperties
+@EnableScheduling
 @PropertySource(value = {
         "application.properties",
+        "application-scheduler.properties",
         "application-persistence.properties",
         "application-security.properties",
         "application-mail-server.properties"})
