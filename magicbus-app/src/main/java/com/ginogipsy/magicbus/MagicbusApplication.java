c273d6a0f3ed9525b87ad976fb2cd2c0a3afab2a
ginogipsy <francesco.grossi92@gmail.com>
2023-01-09 15:05:20 +0100

    created a scheduler and personalized log location

--- magicbus-app/src/main/java/com/ginogipsy/magicbus/MagicbusApplication.java
+++ magicbus-app/src/main/java/com/ginogipsy/magicbus/MagicbusApplication.java
@@ -1,6 +1,7 @@
 package com.ginogipsy.magicbus;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.joda.time.DateTime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
@@ -18,7 +19,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class MagicbusApplication {
 
     public static void main(String[] args) {
+        System.setProperty("currentTimeStamp", DateTime.now().toString());
         SpringApplication.run(MagicbusApplication.class, args);
     }
-
 }
